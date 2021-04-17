5e9a0aa517a4ef731fcb4daa9b7f0df2faf4970c
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-04-16 19:53:12 -0500

    terminamos el modelado

--- Libreria/ProyectoLibreria/gestion/models.py
+++ Libreria/ProyectoLibreria/gestion/models.py
@@ -63,3 +63,70 @@ class ProductoModel(models.Model):
     class Meta:
         db_table = 'productos'
         verbose_name = 'producto'
+
+
+class CabeceraModel(models.Model):
+    # * la cabId tiene que ser pk, no acepta valores nulos, es autoincrementable,
+    # * la cabFecha no acepta valores nulos
+    # * la cabTipo no acepta valores nulos, su verbose_name es 'tipo de la cabecera', agregar opciones para que solamente pueda ser O VENTA O COMPRA (utilice el parametro CHOICES)
+    # ! adicional agregar el nombre de la tabla (cabeceras) y su verbose name (cabecera)
+    TIPO_VENTA = [
+        ('VEN', 'VENTA'),
+        ('COM', 'COMPRA'),
+    ]
+    cabeceraId = models.AutoField(
+        primary_key=True,
+        null=False,
+        db_column='id'
+    )
+    cabeceraFecha = models.DateTimeField(
+        null=False,
+        db_column='fecha'
+    )
+    cabeceraTipo = models.CharField(
+        max_length=45,
+        db_column='tipo',
+        null=False,
+        verbose_name='tipo de la cabecera',
+        choices=TIPO_VENTA,
+    )
+
+    class Meta:
+        db_table = 'cabecera'
+        verbose_name = 'cabecera'
+
+
+class DetalleModel(models.Model):
+    # ! ninguna puede admitir valores nulos
+    # * definir la clase Meta con los valores que crea conveniente
+    detalleId = models.AutoField(
+        primary_key=True,
+        null=False,
+        db_column='id'
+    )
+    detalleCantidad = models.IntegerField(
+        null=False,
+        db_column='cantidad',
+        verbose_name='cantidad',
+        help_text='cantidad del producto a comprar'
+    )
+    detallePrecio = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        db_column="precio",
+        verbose_name='precio',
+        help_text='precio del producto'
+    )
+    producto = models.ForeignKey(to=ProductoModel, on_delete=models.CASCADE,
+                                 related_name='productoDetalles', db_column='producto_id')
+    cabecera = models.ForeignKey(
+        to=CabeceraModel,
+        on_delete=models.CASCADE,
+        related_name='cabeceraDetalles',
+        db_column='cabecera_id',
+        verbose_name='cabecera'
+    )
+
+    class Meta:
+        db_table = 'detalle'
+        verbose_name = 'detalle'
