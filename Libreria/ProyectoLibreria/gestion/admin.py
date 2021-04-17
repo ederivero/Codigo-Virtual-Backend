632b32edacd0acea858dd58e9307b61fef203260
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-04-16 21:39:13 -0500

    libros para reforzamiento del backend

--- Libreria/ProyectoLibreria/gestion/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CategoriaModel, ProductoModel
-
-# https://docs.djangoproject.com/en/3.2/ref/contrib/admin/
-# sirve para registrar un nuevo modelo en el panel administrativo
-admin.site.register(CategoriaModel)
-admin.site.register(ProductoModel)
