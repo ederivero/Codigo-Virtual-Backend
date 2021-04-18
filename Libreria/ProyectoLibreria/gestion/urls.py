5689357e60faba2dcf4c7680b22737088233bcb8
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-04-17 20:54:11 -0500

    crud completo

--- Libreria/ProyectoLibreria/gestion/urls.py
+++ Libreria/ProyectoLibreria/gestion/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListarCategoriaController
+from .views import ListarCategoriaController, CRUDCategoriaController
 
 # ! esta variable se tiene que llamar SI O SI asi:
 urlpatterns = [
     path('categorias', ListarCategoriaController.as_view()),
+    path('categorias/<int:pk>', CRUDCategoriaController.as_view()),
 ]
--- Libreria/ProyectoLibreria/gestion/views.py
+++ Libreria/ProyectoLibreria/gestion/views.py
@@ -1,9 +1,21 @@
-from .models import CategoriaModel
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.response import Response
+from .models import CategoriaModel
+from .serializers import MostrarCategoriasSerializer
 
 
-class ListarCategoriaController(ListAPIView):
+# https://www.django-rest-framework.org/api-guide/generic-views/#concrete-view-classes
+# C => Create (Crear)
+# R => Read   (Leer)
+# U => Update (Actualizar)
+# D => Delete (Eliminar)
+class ListarCategoriaController(ListCreateAPIView):
     # el atributo queryset es el encargado de hacer la consulta a la base de datos para rellenar la respuesta en la vista generica
     # SELECT * FROM CATEGORIAS (sentencia SQL) (sentencias RAW)
     queryset = CategoriaModel.objects.all()
+    serializer_class = MostrarCategoriasSerializer
+
+
+class CRUDCategoriaController(RetrieveUpdateDestroyAPIView):
+    queryset = CategoriaModel.objects.all()
+    serializer_class = MostrarCategoriasSerializer
