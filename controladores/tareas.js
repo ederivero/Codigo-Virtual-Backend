c0d5bc41c38434e33676f613452090db75273da1
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-04-18 22:34:28 -0500

    avance semana 05

--- controladores/tareas.js
+++ controladores/tareas.js
@@ -1,14 +1,44 @@
 // es el encargo de hacer la logica cuando se llame a determinada ruta y con un determinado metodo (GET, POST, PUT)
-const tareas = [];
 // CRUD
 // siempre todo controlador recibe un REQUEST (req) y un RESPONSE (res), ADICIONALMENTE A ELLO si usamos middlewares recibimos un tercer parametro opcional llamado NEXT (next) que es el encargado de pasar al siguiente controlador
 // el request es todo lo que me va a mandar el client
 // el response es la forma en la cual le voy a responder al cliente
 // se puede responder mediante un json (.json()) un texto (.send()) un estado (.status())
+const tareas = [
+  {
+    nombre: "Hacer el foro",
+    estado: true,
+  },
+  {
+    nombre: "Hacer el ejercicio de Flask",
+    estado: false,
+  },
+];
 export const crearTarea = (req, res) => {
   console.log(req.body);
   tareas.push(req.body);
   return res.json({
     content: tareas[tareas.length - 1],
   });
+};
+
+export const listarTareas = (req, res) => {
+  return res.json({
+    content: tareas,
+  });
+};
+
+export const listarTareaPorId = (req, res) => {
+  // mediante el id mandado por la url buscar si existe esa posicion en el arreglo tareas y si la hay mostrar el contenido, caso contrario, indicar que no se encuentra esa tarea
+  console.log(req.params);
+  const { id } = req.params;
+  if (tareas[id - 1]) {
+    return res.json({
+      content: tareas[id - 1],
+    });
+  } else {
+    return res.status(404).json({
+      message: "Not found",
+    });
+  }
 };
--- rutas/tareas.js
+++ rutas/tareas.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
-import { crearTarea } from "../controladores/tareas";
+import {
+  crearTarea,
+  listarTareas,
+  listarTareaPorId,
+} from "../controladores/tareas";
 
 export const tareas_router = Router();
 
-tareas_router.route("/tareas").post(crearTarea);
+tareas_router.route("/tareas").post(crearTarea).get(listarTareas);
+// para hacer un parametro x url DINAMICO simplemente definimos el nombre de esa variable pero con ":" para que express sepa que sera dinamico
+tareas_router.route("/tarea/:id").get(listarTareaPorId);
