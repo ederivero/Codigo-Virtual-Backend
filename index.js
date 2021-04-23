35ef652ea3c9bd6614d8d8b4f325a34cc3548e4e
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-04-22 19:05:44 -0500

    limpieza de la rama

--- index.js
+++
@@ -1,6 +0,0 @@
-// con js puro:
-// const { Server } = require("./config/Server");
-// con babelrc:
-import { Server } from "./config/Server";
-const objServer = new Server();
-objServer.iniciarServidor();
