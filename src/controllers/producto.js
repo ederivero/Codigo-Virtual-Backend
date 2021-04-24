15e32cb24033fe04c6d5436641e0a4edee49481e
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-04-23 22:34:00 -0500

    agregue return producto controller

--- src/controllers/producto.js
+++ src/controllers/producto.js
@@ -4,7 +4,7 @@ import { Producto } from "../config/relaciones";
 export const crearProducto = async (req, res) => {
   // cuando creamos un nuevo registro este retornara el registro creado en la bd
   const nuevoProducto = await Producto.create(req.body);
-  res.status(201).json({
+  return res.status(201).json({
     success: true,
     content: nuevoProducto,
     message: "Producto creado exitosamente",
