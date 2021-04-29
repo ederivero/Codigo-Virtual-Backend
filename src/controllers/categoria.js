40da4fe35140f8e646fcae171ff0bd2a0cf2811f
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-04-28 21:03:30 -0500

    avance jwt

--- src/controllers/categoria.js
+++ src/controllers/categoria.js
@@ -6,6 +6,20 @@ export const crearCategoria = async (req, res) => {
   // retornar un estado 500 si hubo algun error (try-catch)
   // mandar un screenshot del codigo y del postman
   try {
+    // antes de agregar la nueva categoria primero validar que no exista, si existe, entonces no guardar y retornar un status BAD REQUEST (400) indicando que la categoria ya existe
+    const { categoriaNombre } = req.body;
+    const coincidencia = await Categoria.findOne({
+      where: {
+        categoriaNombre,
+      },
+    });
+    if (coincidencia) {
+      return res.status(400).json({
+        success: false,
+        content: null,
+        message: "categoria ya existe",
+      });
+    }
     const nuevaCategoria = await Categoria.create(req.body);
     res.status(201).json({
       success: true,
