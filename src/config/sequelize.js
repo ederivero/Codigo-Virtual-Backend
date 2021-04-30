563447fbb7d9b005dbf0c069f35788edb219167f
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-04-29 19:42:01 -0500

    modifique cadena de conexion

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,19 +1,28 @@
 import { Sequelize } from "sequelize";
 
-export const conexion = new Sequelize(
-  "almacen", // database name
-  "root", // username
-  "root", // password
-  {
-    dialect: "mysql", // tambien podemos usar dialectos para pgadmin, sqlserver, sqlite3, mariadb, mysql
-    // /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */ para sqlite se usa connection URI
-    host: "127.0.0.1",
-    port: 3306,
-    timezone: "-05:00", // no funciona en SQLITE
-    dialectOptions: {
-      // sirve para que al momento de mostrar las fechas, automaticamente las convierta en string y no tener que hacer una conversion manual
-      dateStrings: true,
-    },
-    logging: false,
-  }
-);
+export const conexion = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL, {
+      dialect: "mysql",
+      timezone: "-05:00",
+      logging: false,
+      dialectOptions: {
+        dateStrings: true,
+      },
+    })
+  : new Sequelize(
+      "almacen", // database name
+      "root", // username
+      "root", // password
+      {
+        dialect: "mysql", // tambien podemos usar dialectos para pgadmin, sqlserver, sqlite3, mariadb, mysql
+        // /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */ para sqlite se usa connection URI
+        host: "127.0.0.1",
+        port: 3306,
+        timezone: "-05:00", // no funciona en SQLITE
+        dialectOptions: {
+          // sirve para que al momento de mostrar las fechas, automaticamente las convierta en string y no tener que hacer una conversion manual
+          dateStrings: true,
+        },
+        logging: false,
+      }
+    );
