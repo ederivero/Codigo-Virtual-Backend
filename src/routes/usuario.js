c9dca710faa96a966787290ff03c5f25385fdde9
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-04-30 22:18:16 -0500

    finalizacion login queda pediente la implementacion de JWT

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -4,3 +4,4 @@ import * as usuario_controller from "../controllers/usuario";
 export const usuario_router = Router();
 
 usuario_router.post("/registro", usuario_controller.registro);
+usuario_router.post("/login", usuario_controller.login);
