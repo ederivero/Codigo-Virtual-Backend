68088c6999f45ed0e88510519d5ca4dcdf3f2da8
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-05-07 21:24:30 -0500

    agrege js

---
+++ subirImagenes/src.js
@@ -0,0 +1,6 @@
+const nombre = document.getElementById("inputNombre");
+const apellido = document.getElementById("inputApellido");
+const sexo = document.getElementById("inputSexo");
+const direccion = document.getElementById("inputDireccion");
+const fotografia = document.getElementById("inputFotografia");
+const telefono = document.getElementById("inputTelefono");
