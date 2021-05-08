adc23d79d9ef46d78cde684a7405d827cdd40e88
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-05-07 19:49:25 -0500

    modificacion subirImagen

--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -1,5 +1,4 @@
 export const crearUsuario = (req, res) => {
-  // si subimos un solo archivo (single) usaremos el parametro <<req.file>>, caso contrario, si subimos varios archivos (array) usaremos el parametro <<req.files>>
   return res.json({
     success: true,
   });
