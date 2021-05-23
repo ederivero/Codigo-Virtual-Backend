9c10194f803ffb3579bd09414cf02f3352e1958a
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-05-22 20:05:59 -0500

    modificaciones

--- paint/src/config/server.js
+++ paint/src/config/server.js
@@ -37,7 +37,6 @@ export default class Server {
     this.io.on("connect", (cliente) => {
       console.log(`Se conecto el cliente ${cliente.id}`);
       cliente.on("coordenada", (data) => {
-        console.log(data);
         ingresarCoordenada(data, cliente);
       });
     });
