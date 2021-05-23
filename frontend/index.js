4cd11293a11819e7203994571c318b39ddb5f298
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2021-05-22 19:59:13 -0500

    sockets funcionando con bd

--- frontend/index.js
+++ frontend/index.js
@@ -4,24 +4,32 @@ function setup() {
   createCanvas(300, 300);
   background(100);
   socket.on("coordenadas", (data) => {
-    console.log(data);
-    fill(0, 0, 255);
-    noStroke();
-    ellipse(data.x, data.y, 15, 15);
+    data.forEach((registro) => {
+      registro.coordenadas.forEach((coordenada) => {
+        fill(registro.color);
+        noStroke();
+        ellipse(
+          coordenada.x.$numberDecimal,
+          coordenada.y.$numberDecimal,
+          15,
+          15
+        );
+      });
+    });
   });
 }
 // mouseDragged => solamente se pintara cuando hagamos click
 // draw => basta con pasar el cursor por el canvas
 function mouseDragged() {
-  fill(112, 239, 65);
+  fill("#00000");
   noStroke(); // sirve para no bordear el lienzo
   ellipse(mouseX, mouseY, 15, 15);
   enviarPunto(mouseX, mouseY);
 }
 
 const enviarPunto = (posX, posY) => {
-  console.log(`Posicion x: ${posX}`);
-  console.log(`Posicion Y: ${posY}`);
+  // console.log(`Posicion x: ${posX}`);
+  // console.log(`Posicion Y: ${posY}`);
   const data = {
     x: posX,
     y: posY,
